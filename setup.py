47e87ab07ff848e195d5cb2611b372f22c9343a5
Marko Toplak <marko.toplak@gmail.com>
2016-03-07 16:19:24 +0100

    Start from an orange3-example-addon

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+ENTRY_POINTS = {
+    # Entry point used to specify packages containing tutorials accessible
+    # from welcome screen. Tutorials are saved Orange Workflows (.ows files).
+    'orange.widgets.tutorials': (
+        # Syntax: any_text = path.to.package.containing.tutorials
+        'exampletutorials = orangecontrib.example.tutorials',
+    ),
+
+    # Entry point used to specify packages containing widgets.
+    'orange.widgets': (
+        # Syntax: category name = path.to.package.containing.widgets
+        # Widget category specification can be seen in
+        #    orangecontrib/example/widgets/__init__.py
+        'Infrared = orangecontrib.infrared.widgets',
+    ),
+}
+
+KEYWORDS = (
+    # [PyPi](https://pypi.python.org) packages with keyword "orange3 add-on"
+    # can be installed using the Orange Add-on Manager
+    'orange3 add-on',
+)
+
+if __name__ == '__main__':
+    setup(
+        name="Orange-Infrared",
+        description='',
+        author='',
+        author_email='',
+        packages=find_packages(),
+        install_requires=[
+            'Orange',
+        ],
+        entry_points=ENTRY_POINTS,
+        keywords=KEYWORDS,
+        namespace_packages=['orangecontrib'],
+        include_package_data=True,
+        zip_safe=False,
+    )
