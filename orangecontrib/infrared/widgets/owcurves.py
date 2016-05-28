9fd182d3d9a3cff7851570afa8a895351de1fb37
borondics <borondics.accounts@gmail.com>
2016-05-28 17:14:30 +0200

    Pad the y axis by 2% in all views

--- orangecontrib/infrared/widgets/owcurves.py
+++ orangecontrib/infrared/widgets/owcurves.py
@@ -384,6 +384,11 @@ class CurvePlot(QWidget):
                        for x,y in self.curves)
 
             self.plot.vb.setYRange(ymin, ymax, padding=0.0)
+            self.pad_current_view_y()
+
+    def pad_current_view_y(self):
+        qrect = self.plot.vb.targetRect()
+        self.plot.vb.setYRange(qrect.bottom(), qrect.top(), padding=0.02)
 
     def show_average(self):
         self.viewtype = AVERAGE
@@ -423,9 +428,11 @@ class CurvePlot(QWidget):
             if self.viewtype == INDIVIDUAL:
                 self.show_individual()
                 self.plot.vb.autoRange()
+                self.pad_current_view_y()
             elif self.viewtype == AVERAGE:
                 self.show_average()
                 self.plot.vb.autoRange()
+                self.pad_current_view_y()
 
     def set_data_subset(self, ids):
         self.subset_ids = set(ids) if ids is not None else set()
