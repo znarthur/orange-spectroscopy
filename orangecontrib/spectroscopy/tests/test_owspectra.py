d53be5536111e724be6419043a25864f704b2919
Marko Toplak <marko.toplak@gmail.com>
2018-06-21 18:40:47 +0200

    Fix tests to work with changed order of group colors
    Fixes problems after Orange commit c13d5a6b88312fc5d3e91f79a344c3bcfdba0c58:
    OWScatterOWScatterPlotGraph: Match group colors with marker colors

--- orangecontrib/spectroscopy/tests/test_owspectra.py
+++ orangecontrib/spectroscopy/tests/test_owspectra.py
@@ -346,7 +346,8 @@ class TestOWSpectra(WidgetTest):
         newvars = out.domain.variables + out.domain.metas
         oldvars = data.domain.variables + data.domain.metas
         group_at = [a for a in newvars if a not in oldvars][0]
-        out = out[np.flatnonzero(out.transform(Orange.data.Domain([group_at])).X != 0)]
+        unselected = group_at.to_val("Unselected")
+        out = out[np.flatnonzero(out.transform(Orange.data.Domain([group_at])).X != unselected)]
         self.assertEqual(len(out), 4)
         np.testing.assert_equal([o for o in out], [data[i] for i in [1, 2, 3, 4]])
         np.testing.assert_equal([o[group_at].value for o in out], ["G1", "G2", "G3", "G3"])
