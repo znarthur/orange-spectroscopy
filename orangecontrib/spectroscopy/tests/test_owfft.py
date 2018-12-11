15b785c95468ee87c066e1682c084cc3be8be4ed
Stuart Read <stuart.read@lightsource.ca>
2018-12-11 15:54:27 -0600

    owfft: use laser wavenumber and sampling interval metadata if present
    Closes #261

--- orangecontrib/spectroscopy/tests/test_owfft.py
+++ orangecontrib/spectroscopy/tests/test_owfft.py
@@ -7,7 +7,25 @@ class TestOWFFT(WidgetTest):
 
     def setUp(self):
         self.widget = self.create_widget(OWFFT)
+        self.ifg_single = Orange.data.Table("IFG_single.dpt")
+        self.ifg_seq = Orange.data.Table("agilent/4_noimage_agg256.seq")
 
     def test_load_unload(self):
-        self.send_signal("Interferogram", Orange.data.Table("IFG_single.dpt"))
+        self.send_signal("Interferogram", self.ifg_single)
         self.send_signal("Interferogram", None)
+
+    def test_laser_metadata(self):
+        """ Test dx in presence/absence of laser metadata """
+        self.send_signal("Interferogram", self.ifg_seq)
+        self.assertEqual(self.widget.dx, (1 / 1.57980039e+04 / 2) * 4)
+        self.send_signal("Interferogram", self.ifg_single)
+        self.assertEqual(self.widget.dx, (1 / self.widget.laser_wavenumber / 2))
+
+    def test_respect_custom_dx(self):
+        """ Setting new data should not overwrite custom dx value """
+        self.send_signal("Interferogram", self.ifg_single)
+        self.widget.dx_HeNe = False
+        self.widget.dx = 5
+        self.widget.dx_changed()
+        self.send_signal("Interferogram", self.ifg_single)
+        self.assertEqual(self.widget.dx, 5)
