e82c66eadd0b0513d4252671eb5c5fd160925a00
Marko Toplak <marko.toplak@gmail.com>
2019-05-13 11:54:50 +0200

    owpreprocess: adding zero points for baseline correction

--- orangecontrib/spectroscopy/widgets/owpreprocess.py
+++ orangecontrib/spectroscopy/widgets/owpreprocess.py
@@ -447,6 +447,9 @@ class BaselineEditor(BaseEditorOrange):
     Baseline subtraction.
     """
 
+    MINLIM_DEFAULT = 0.
+    MAXLIM_DEFAULT = 1.
+
     def __init__(self, parent=None, **kwargs):
         super().__init__(parent, **kwargs)
         self.controlArea.setLayout(QVBoxLayout())
@@ -471,9 +474,93 @@ class BaselineEditor(BaseEditorOrange):
         form.addRow("Peak Direction", self.peakcb)
         form.addRow("Background Action", self.subcb)
 
+        self.controlArea.layout().addLayout(form)
+
+        self.ranges_box = gui.vBox(self.controlArea)  # container for ranges
+
+        self.range_button = QPushButton("", autoDefault=False)
+        self.range_button.clicked.connect(self.add_point)
+        self.controlArea.layout().addWidget(self.range_button)
+
+        self.reference_curve = pg.PlotCurveItem()
+        self.reference_curve.setPen(pg.mkPen(color=QColor(Qt.red), width=2.))
+        self.reference_curve.setZValue(10)
+
+        self.user_changed = False
+
         self._adapt_ui()
 
-        self.controlArea.layout().addLayout(form)
+    def activateOptions(self):
+        self.parent_widget.curveplot.clear_markings()
+
+        for pair in self._range_widgets():
+            for w in pair:
+                if w.line not in self.parent_widget.curveplot.markings:
+                    w.line.report = self.parent_widget.curveplot
+                    self.parent_widget.curveplot.add_marking(w.line)
+
+    def _set_button_text(self):
+        self.range_button.setText("Select point"
+                                  if self.ranges_box.layout().count() == 0
+                                  else "Add point")
+
+    def _range_widgets(self):
+        for b in layout_widgets(self.ranges_box):
+            yield self._extract_pair(b)
+
+    def add_point(self):
+        pmin, pmax = self.preview_min_max()
+        if len(list(self._range_widgets())) == 0:  # if empty, add two points at the same time
+            lwmin = self.add_range_selection_ui()
+            lwmax = self.add_range_selection_ui()
+            self._extract_pair(lwmin)[0].position = pmin
+            self._extract_pair(lwmax)[0].position = pmax
+        else:
+            lw = self.add_range_selection_ui()
+            self._extract_pair(lw)[0].position = (pmin + pmax) / 2
+        self.edited.emit()  # refresh output
+
+    def _extract_pair(self, container):
+        return list(layout_widgets(container))[:1]
+
+    def add_range_selection_ui(self):
+        linelayout = gui.hBox(self)
+        pmin, pmax = self.preview_min_max()
+        e = XPosLineEdit(label="")
+        e.set_default((pmin+pmax)/2)
+        linelayout.layout().addWidget(e)
+        e.edited.connect(self.edited)
+        e.focusIn.connect(self.activateOptions)
+
+        remove_button = QPushButton(QApplication.style().standardIcon(QStyle.SP_DockWidgetCloseButton),
+                                    "", autoDefault=False)
+        remove_button.clicked.connect(lambda: self.delete_range(linelayout))
+        linelayout.layout().addWidget(remove_button)
+
+        self.ranges_box.layout().addWidget(linelayout)
+        self._set_button_text()
+        return linelayout
+
+    def delete_range(self, box):
+        removed = [box]
+        self.ranges_box.layout().removeWidget(box)
+
+        # if only 1 widget stayed that would be invalid
+        if len(list(layout_widgets(self.ranges_box))) == 1:
+            also_remove = next(layout_widgets(self.ranges_box))
+            self.ranges_box.layout().removeWidget(also_remove)
+            removed.append(also_remove)
+
+        self._set_button_text()
+
+        # remove selection lines
+        curveplot = self.parent_widget.curveplot
+        for r in removed:
+            for w in self._extract_pair(r):
+                if curveplot.in_markings(w.line):
+                    curveplot.remove_marking(w.line)
+
+        self.edited.emit()
 
     def setParameters(self, params):
         self.baseline_type = params.get("baseline_type", 0)
@@ -484,19 +571,38 @@ class BaselineEditor(BaseEditorOrange):
     def _adapt_ui(self):
         # peak direction is only relevant for rubberband
         self.peakcb.setEnabled(self.baseline_type == 1)
+        self._set_button_text()
+
+    def parameters(self):
+        parameters = super().parameters()
+        parameters["zero_points"] = []
+        for pair in self._range_widgets():
+            parameters["zero_points"].append(float(pair[0].position))
+        return parameters
 
     @staticmethod
     def createinstance(params):
         baseline_type = params.get("baseline_type", 0)
         peak_dir = params.get("peak_dir", 0)
         sub = params.get("sub", 0)
+        zero_points = params.get("zero_points", [])
 
         if baseline_type == 0:
-            return LinearBaseline(peak_dir=peak_dir, sub=sub)
+            return LinearBaseline(peak_dir=peak_dir, sub=sub, zero_points=zero_points)
         elif baseline_type == 1:
             return RubberbandBaseline(peak_dir=peak_dir, sub=sub)
         else:
             raise Exception("unknown baseline type")
+
+    def preview_min_max(self):
+        if self.preview_data is not None:
+            x = getx(self.preview_data)
+            if len(x):
+                return min(x), max(x)
+        return self.MINLIM_DEFAULT, self.MAXLIM_DEFAULT
+
+    def set_preview_data(self, data):
+        self.preview_data = data
 
 
 class CurveShiftEditor(BaseEditorOrange):
