8a859ef1a3e8fe1f3e789332ec1fb532da51b4e2
Marko Toplak <marko.toplak@gmail.com>
2020-06-16 20:05:14 +0200

    Release 0.5.4

--- setup.py
+++ setup.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
         long_description=LONG_DESCRIPTION,
         author='Canadian Light Source, Biolab UL, Soleil, Elettra',
         author_email='marko.toplak@gmail.com',
-        version="0.5.3",
+        version="0.5.4",
         packages=PACKAGES,
         package_data=PACKAGE_DATA,
         data_files=DATA_FILES,
