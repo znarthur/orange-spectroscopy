ebe20f58181b6c488e52df9e512545dfa2db2d1e
Marko Toplak <marko.toplak@gmail.com>
2020-07-27 22:25:14 +0200

    Require renishawWiRE>=0.1.8, which fixes compatibility with newer Pillow.

--- setup.py
+++ setup.py
@@ -137,7 +137,7 @@ if __name__ == '__main__':
             'colorcet',
             'h5py',
             'extranormal3',
-            'renishawWiRE',
+            'renishawWiRE>=0.1.8',
         ],
         entry_points=ENTRY_POINTS,
         keywords=KEYWORDS,
