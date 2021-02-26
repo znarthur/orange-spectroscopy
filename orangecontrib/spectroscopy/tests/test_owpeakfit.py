16a895ca1f424c1f864b165c1d36a6075a4e1150
Stuart Read <stuart.read@lightsource.ca>
2021-02-26 14:55:46 -0600

    peakfit: Test editor setting value and multiple peak

--- orangecontrib/spectroscopy/tests/test_owpeakfit.py
+++ orangecontrib/spectroscopy/tests/test_owpeakfit.py
@@ -5,6 +5,7 @@ import numpy as np
 import Orange
 import lmfit
 from Orange.widgets.tests.base import WidgetTest
+from lmfit import Parameters
 
 from orangecontrib.spectroscopy.preprocess import Cut
 from orangecontrib.spectroscopy.tests.spectral_preprocess import wait_for_preview
@@ -103,6 +104,7 @@ class TestVoigtEditor(ModelEditorTest):
     def test_no_interaction(self):
         self.widget.unconditional_commit()
         self.wait_until_finished()
+        self.assertIsInstance(self.editor, self.EDITOR)
         m = self.get_model_single()
         self.assertIsInstance(m, self.EDITOR.model)
 
@@ -111,3 +113,71 @@ class TestVoigtEditor(ModelEditorTest):
         params = self.get_params_single(m)
         for p in self.editor.parameters():
             self.assertIn(f"{m.prefix}{p}", params)
+
+    def test_set_center(self):
+        e = self.editor
+        e.set_value('center', 1655)
+        e.edited.emit()
+        m = self.get_model_single()
+        params = self.get_params_single(m)
+        c_p = f'{m.prefix}center'
+        self.assertEqual(1655, params[c_p].value)
+
+
+class TestVoigtEditorMulti(PreprocessorEditorTest):
+
+    def setUp(self):
+        self.widget = self.create_widget(OWPeakFit)
+        self.editors = [self.add_editor(VoigtModelEditor, self.widget) for _ in range(4)]
+        self.data = COLLAGEN
+        self.send_signal(self.widget.Inputs.data, self.data)
+
+    def matched_models(self):
+        pcs = [1400, 1457, 1547, 1655]
+        mlist = [lmfit.models.VoigtModel(prefix=f"v{i}_") for i in range(len(pcs))]
+        model = reduce(lambda x, y: x + y, mlist)
+        params = model.make_params()
+        for i, center in enumerate(pcs):
+            p = f"v{i}_"
+            dx = 20
+            # params[p + "center"].set(value=center, min=center - dx, max=center + dx)
+            params[p + "center"].set(value=center)
+            # params[p + "sigma"].set(max=50)
+            # params[p + "amplitude"].set(min=0.0001)
+            # Set editor to same value
+            e = self.editors[i]
+            e.set_value("center", center)
+            # TODO min/max/dx not possible yet from editor
+            # e.set_value("sigma", max=50)
+            e.edited.emit()
+        return model, params
+
+    def test_same_params(self):
+        model, params = self.matched_models()
+        m_def = [self.widget.preprocessormodel.item(i)
+                 for i in range(self.widget.preprocessormodel.rowCount())]
+        n = len(m_def)
+        ed_m_list = []
+        ed_params = Parameters()
+        for i in range(n):
+            item = m_def[i]
+            m = create_model(item, i)
+            p = prepare_params(item, m)
+            ed_m_list.append(m)
+            ed_params.update(p)
+        ed_model = reduce(lambda x, y: x+y, ed_m_list)
+
+        self.assertEqual(model.name, ed_model.name)
+        self.assertEqual(set(params), set(ed_params))
+
+    def test_same_output(self):
+        model, params = self.matched_models()
+
+        out_fit = fit_peaks(self.data, model, params)
+
+        self.widget.unconditional_commit()
+        self.wait_until_finished(timeout=10000)
+        out = self.get_output(self.widget.Outputs.fit)
+
+        self.assertEqual(out_fit.domain.attributes, out.domain.attributes)
+        np.testing.assert_array_equal(out_fit.X, out.X)
