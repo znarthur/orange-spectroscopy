21d8b239affb8716aa21f386eebb629066b11f13
Marko Toplak <marko.toplak@gmail.com>
2021-08-10 12:21:03 +0200

    MeEMSC editor: number of components, fixed iterations

--- orangecontrib/spectroscopy/tests/test_editor_me_emsc.py
+++ orangecontrib/spectroscopy/tests/test_editor_me_emsc.py
@@ -14,18 +14,55 @@ class TestMeEMSCEditor(PreprocessorEditorTest):
 
     def setUp(self):
         self.widget = self.create_widget(OWPreprocess)
-        self.editor = self.add_editor(MeEMSCEditor, self.widget)
+        self.editor = self.add_editor(MeEMSCEditor, self.widget)  # type: MeEMSCEditor
+        self.editor.controls.max_iter.setValue(3)
         # FIXME: current ME-EMSC can not handle same data and reference
         self.data = SMALL_COLLAGEN[1:3]
         self.send_signal(self.widget.Inputs.data, self.data)
 
+    def commit_get_preprocessor(self):
+        self.widget.unconditional_commit()
+        self.wait_until_finished()
+        return self.get_preprocessor()
+
     def test_no_interaction(self):
         reference = SMALL_COLLAGEN[:1]
         self.send_signal(self.widget.Inputs.reference, reference)
-        self.widget.unconditional_commit()
-        self.wait_until_finished()
-        p = self.get_preprocessor()
+        p = self.commit_get_preprocessor()
         self.assertIsInstance(p, ME_EMSC)
+
+    def test_ncomp(self):
+        reference = SMALL_COLLAGEN[:1]
+        self.send_signal(self.widget.Inputs.reference, reference)
+
+        p = self.commit_get_preprocessor()
+        self.assertFalse(self.editor.controls.ncomp.isEnabled())
+        self.assertEqual(p.ncomp, 6)  # for this data set
+
+        self.editor.controls.autoset_ncomp.click()
+        self.editor.controls.ncomp.setValue(4)
+        p = self.commit_get_preprocessor()
+        self.assertTrue(self.editor.controls.ncomp.isEnabled())
+        self.assertEqual(p.ncomp, 4)  # for this data set
+
+        self.editor.controls.autoset_ncomp.click()
+        p = self.commit_get_preprocessor()
+        self.assertFalse(self.editor.controls.ncomp.isEnabled())
+        self.assertEqual(p.ncomp, 6)  # for this data set
+
+    def test_iterations(self):
+        reference = SMALL_COLLAGEN[:1]
+        self.send_signal(self.widget.Inputs.reference, reference)
+
+        p = self.commit_get_preprocessor()
+        self.assertEqual(p.maxNiter, 3)
+        self.assertEqual(p.fixedNiter, False)
+
+        self.editor.controls.max_iter.setValue(4)
+        self.editor.controls.fixed_iter.click()
+        p = self.commit_get_preprocessor()
+        self.assertEqual(p.maxNiter, 4)
+        self.assertEqual(p.fixedNiter, 4)
 
     def test_migrate_smoothing(self):
         name = "orangecontrib.spectroscopy.preprocess.me_emsc.me_emsc"
--- orangecontrib/spectroscopy/widgets/preprocessors/me_emsc.py
+++ orangecontrib/spectroscopy/widgets/preprocessors/me_emsc.py
@@ -9,7 +9,10 @@ from orangecontrib.spectroscopy.widgets.preprocessors.emsc import EMSCEditor
 
 class MeEMSCEditor(EMSCEditor):
     MAX_ITER_DEFAULT = 30
+    FIXED_ITER_DEFAULT = False
     OUTPUT_MODEL_DEFAULT = False
+    NCOMP_DEFAULT = 7
+    AUTOSET_NCOMP_DEFAULT = True
 
     def __init__(self, parent=None, **kwargs):
         BaseEditorOrange.__init__(self, parent, **kwargs)
@@ -22,9 +25,21 @@ class MeEMSCEditor(EMSCEditor):
         self.preview_data = None
 
         self.max_iter = self.MAX_ITER_DEFAULT
+        self.fixed_iter = self.FIXED_ITER_DEFAULT
+        self.ncomp = self.NCOMP_DEFAULT
+        self.autoset_ncomp = self.AUTOSET_NCOMP_DEFAULT
 
         gui.spin(self.controlArea, self, "max_iter", label="Max iterations", minv=0, maxv=100,
                  controlWidth=50, callback=self.edited.emit)
+        gui.checkBox(self.controlArea, self, "fixed_iter", label="Use fixed number of iterations",
+                     callback=self.edited.emit)
+
+        self.comp_spin = gui.spin(self.controlArea, self, "ncomp", label="Components",
+                                  minv=3, maxv=15,
+                                  controlWidth=50, callback=self.edited.emit)
+        gui.checkBox(self.controlArea, self, "autoset_ncomp",
+                     label="Automatically set components",
+                     callback=lambda: (self._auto_click(), self.edited.emit()))
 
         self.reference_info = QLabel("", self)
         self.controlArea.layout().addWidget(self.reference_info)
@@ -33,10 +48,14 @@ class MeEMSCEditor(EMSCEditor):
         gui.checkBox(self.controlArea, self, "output_model", "Output EMSC model",
                      callback=self.edited.emit)
 
+        self._auto_click()
         self._init_regions()
         self._init_reference_curve()
 
         self.user_changed = False
+
+    def _auto_click(self):
+        self.comp_spin.setEnabled(not self.autoset_ncomp)
 
     def setParameters(self, params):
         if params:
@@ -44,8 +63,12 @@ class MeEMSCEditor(EMSCEditor):
 
         self.max_iter = params.get("max_iter", self.MAX_ITER_DEFAULT)
         self.output_model = params.get("output_model", self.OUTPUT_MODEL_DEFAULT)
+        self.fixed_iter = params.get("fixed_iter", self.FIXED_ITER_DEFAULT)
+        self.ncomp = params.get("ncomp", self.NCOMP_DEFAULT)
+        self.autoset_ncomp = params.get("autoset_ncomp", self.AUTOSET_NCOMP_DEFAULT)
         self._set_range_parameters(params)
 
+        self._auto_click()
         self.update_reference_info()
         self.update_weight_curve(params)
 
@@ -53,6 +76,13 @@ class MeEMSCEditor(EMSCEditor):
     def createinstance(cls, params):
         max_iter = params.get("max_iter", cls.MAX_ITER_DEFAULT)
         output_model = params.get("output_model", cls.OUTPUT_MODEL_DEFAULT)
+        fixed_iter = params.get("fixed_iter", cls.FIXED_ITER_DEFAULT)
+        ncomp = params.get("ncomp", cls.NCOMP_DEFAULT)
+        autoset_ncomp = params.get("autoset_ncomp", cls.AUTOSET_NCOMP_DEFAULT)
+
+        if fixed_iter:
+            fixed_iter = max_iter
+        ncomp = False if autoset_ncomp else ncomp
 
         weights = cls._compute_weights(params)
 
@@ -61,4 +91,5 @@ class MeEMSCEditor(EMSCEditor):
             return lambda data: data[:0]  # return an empty data table
         else:
             return ME_EMSC(reference=reference, weights=weights, max_iter=max_iter,
+                           fixed_iter=fixed_iter, ncomp=ncomp,
                            output_model=output_model)
