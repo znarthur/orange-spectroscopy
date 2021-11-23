08fec4465ea530aed12d9b9c9052062336df0c53
Marko Toplak <marko.toplak@gmail.com>
2021-11-23 15:28:00 +0100

    Release 0.6.2
    [ENH] Visualizations: selection outputs groups
    [ENH] Peak Fit: finally responsive (with multiprocessing)
    [FIX] Peakfit: clear preview markings before calculation
    [FIX] Non-decimal-limited and typing-friendly spin-boxes
    Adapt for Orange's table-lock

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ if __name__ == '__main__':
         long_description_content_type='text/markdown',
         author='Canadian Light Source, Biolab UL, Soleil, Elettra',
         author_email='marko.toplak@gmail.com',
-        version="0.6.1",
+        version="0.6.2",
         packages=PACKAGES,
         package_data=PACKAGE_DATA,
         data_files=DATA_FILES,
