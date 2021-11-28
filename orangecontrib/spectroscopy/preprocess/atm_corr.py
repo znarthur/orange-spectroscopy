e3455c51d94af22d4afaa05adeb26f35a9cd3a75
Carl Troein <carl@thep.lu.se>
2021-11-28 15:08:59 +0100

    atm-corr: Reworked spline code, added option for window width

--- orangecontrib/spectroscopy/preprocess/atm_corr.py
+++ orangecontrib/spectroscopy/preprocess/atm_corr.py
@@ -11,9 +11,27 @@ from orangecontrib.spectroscopy.preprocess.utils import SelectColumn, CommonDoma
 
 
 class _AtmCorr(CommonDomainOrderUnknowns):
+    """
+    Atmospheric gas correction. Removes reference spectrum (or spectra) from
+    the input spectra.
 
+    correct_range and spline_range are lists of [x1, x2] that define
+    non-overlapping x-value ranges that are individually corrected. For each
+    range in correct_ranges and for each spectrum, the amount of reference
+    subtracted from (or added to) the spectrum is chosen such that the
+    resulting spectrum is maximally smooth; the sum of squares of the first
+    derivative (differences between consecutive points) is minimized.
+    If multiple references are used (mean_reference=False), the subtracted
+    reference is a weighted sum of all the references. (In practice, this may
+    be a poor replacement for arbitrary linear combinations of the references.)
+    Optionally, the ranges in correct_ranges are smoothed with a Savitzky-Golay
+    filter (poly-order 3, window width smooth_win).
+    For each range in spline_ranges, data are replaced with a non-overshooting
+    spline that merges gradually with the data at its edges (using a Tukey
+    window with alpha=0.3).
+    """
     def __init__(self, reference, *, correct_ranges, spline_ranges,
-                 smooth_win, mean_reference, domain):
+                 smooth_win, spline_base_win, mean_reference, domain):
         super().__init__(domain)
         self.reference = reference
         if correct_ranges is not None:
@@ -25,6 +43,7 @@ class _AtmCorr(CommonDomainOrderUnknowns):
         else:
             self.spline_ranges = [[2190, 2480]]
         self.smooth_win = smooth_win
+        self.spline_base_win = spline_base_win
         self.mean_reference = mean_reference
 
     def transformed(self, X, wavenumbers):
@@ -38,81 +57,78 @@ class _AtmCorr(CommonDomainOrderUnknowns):
 
         def find_wn_ranges(wn, ranges):
             # Find indexes of a list of ranges of wavenumbers.
+            if not len(ranges):
+                return np.zeros((0, 0))
             ranges = np.asarray(ranges)
             r = np.stack((np.searchsorted(wn, ranges[:,0]),
                           np.searchsorted(wn, ranges[:,1], 'right')), 1)
-            r[r == len(wn)] = len(wn) - 1
+            # r[r == len(wn)] = len(wn) - 1
             return r
 
         y = X.copy()
 
-        corr_ranges = len(self.correct_ranges)
-        if corr_ranges:
-            ranges = find_wn_ranges(wavenumbers, self.correct_ranges)
-
+        ranges = find_wn_ranges(wavenumbers, self.correct_ranges)
+        ranges = [[p, q] for p, q in ranges if q - p > 1]
+        if ranges:
             if self.mean_reference:
                 atms = np.atleast_2d(spectra_mean(self.reference.X))
-                atms = interpolate_to_data(getx(self.reference), atms)
             else:
                 atms = np.atleast_2d(self.reference.X)
-                atms = interpolate_to_data(getx(self.reference), atms)
-
+            atms = interpolate_to_data(getx(self.reference), atms)
 
             # remove baseline in reference (skip this?)
-            for i in range(corr_ranges):
-                p, q = ranges[i]
-                if q - p < 2: continue
-                atms[:, p:q] -= interp1d(wavenumbers[[p,q-1]], atms[:, [p,q-1]])(wavenumbers[p:q])
+            for p, q in ranges:
+                atms[:, p:q] -= interp1d(wavenumbers[[p,q-1]],
+                                         atms[:, [p,q-1]])(wavenumbers[p:q])
 
             # Basic removal of atmospheric spectrum
             dy = X[:,:-1] - X[:,1:]
-            az = np.zeros((len(atms), len(X), corr_ranges))
+            az = np.zeros((len(atms), len(X), len(ranges)))
             for ia, atm in enumerate(atms):
                 dh = atm[:-1] - atm[1:]
                 dh2 = np.cumsum(dh * dh)
                 dhdy = np.cumsum(dy * dh, 1)
-                for i in range(corr_ranges):
+                for i in range(len(ranges)):
                     p, q = ranges[i]
-                    if q - p < 2: continue
-                    r = q - 2 if q <= len(wavenumbers) else q - 1
+                    r = q - 2
                     az[ia, :, i] = \
                         ((dhdy[:,r] - dhdy[:,p-1]) / (dh2[r] - dh2[p-1])) \
                             if p > 0 else (dhdy[:,r] / dh2[r])
-            az = az * az / az.sum(0)
+            az2sum = (az * az).sum(0)
+            az = az**3 / az2sum
             for ia, atm in enumerate(atms):
-                for i in range(corr_ranges):
+                for i in range(len(ranges)):
                     p, q = ranges[i]
-                    if q - p < 2: continue
                     y[:, p:q] -= az[ia, :, i, None] @ atm[None, p:q]
 
         # Smoothing of atmospheric regions
-        if self.smooth_win > 3:
-            for i in range(corr_ranges):
-                p, q = ranges[i]
-                if q - p >= self.smooth_win:
-                    y[:, p:q] = savgol_filter(y[:, p:q], self.smooth_win, 3, axis=1)
+        for p, q in ranges:
+            if q - p >= self.smooth_win > 3:
+                y[:, p:q] = savgol_filter(y[:, p:q], self.smooth_win, 3, axis=1)
 
         # Replace (CO2) region(s) with spline(s)
-        # ranges = find_wn_ranges(wavenumbers, self.spline_ranges)
+        tukey_smoothness = 0
         for srange in self.spline_ranges:
-            w = (srange[1] - srange[0]) * .24
-            rng = np.array([[srange[0], srange[0] + w],
-                            [srange[1] - w, srange[1]]])
-            rngm = rng.mean(1)
-            rngd = rngm[1] - rngm[0]
-            cr = find_wn_ranges(wavenumbers, rng).flatten()
+            P, Q = find_wn_ranges(wavenumbers, [srange]).flatten()
+            Q = min(Q, len(wavenumbers) - 1)
+            # TODO: Handle small Q - P better
+            if P >= Q:
+                continue
+            Pw, Qw = wavenumbers[[P, Q]]
+            bw = int(self.spline_base_win) // 2
+            cr = [max(P - bw, 0), min(P + bw + 1, len(wavenumbers)),
+                  max(Q - bw, 0), min(Q + bw + 1, len(wavenumbers))]
 
-            if cr[1] - cr[0] > 2 and cr[3] - cr[2] > 2:
-                a = np.empty((4, len(y)))
-                a[0:2,:] = np.polyfit((wavenumbers[cr[0]:cr[1]]-rngm[0])/rngd,
-                                      y[:,cr[0]:cr[1]].T, deg=1)
-                a[2:4,:] = np.polyfit((wavenumbers[cr[2]:cr[3]]-rngm[1])/rngd,
-                                      y[:,cr[2]:cr[3]].T, deg=1)
-                P, Q = find_wn_ranges(wavenumbers, rngm[None,:])[0]
-                t = np.interp(wavenumbers[P:Q], wavenumbers[[P,Q]], [1, 0])
-                tt = np.array([-t**3+t**2, -2*t**3+3*t**2, -t**3+2*t**2-t, 2*t**3-3*t**2+1])
-                pt = a.T @ tt
-                y[:, P:Q] += (pt - y[:, P:Q]) * tukey(len(t), .3)
+            a = np.empty((4, len(y)))
+            a[0:2,:] = np.polyfit(wavenumbers[cr[0]:cr[1]] - Pw,
+                                  y[:,cr[0]:cr[1]].T, deg=1)
+            a[2:4,:] = np.polyfit(wavenumbers[cr[2]:cr[3]] - Qw,
+                                  y[:,cr[2]:cr[3]].T, deg=1)
+            a[::2,:] = a[::2,:] * (Qw - Pw)
+            t = np.interp(wavenumbers[P:Q+1], [Pw, Qw], [0, 1])
+            tt = np.array([t**3-2*t**2+t, 2*t**3-3*t**2+1, t**3-t**2, -2*t**3+3*t**2])
+            pt = a.T @ tt
+            y[:, P:Q+1] += (pt - y[:, P:Q+1]) * tukey(len(t), tukey_smoothness)
 
         return y
 
@@ -120,13 +136,15 @@ class _AtmCorr(CommonDomainOrderUnknowns):
 class AtmCorr(Preprocess):
 
     def __init__(self, reference=None, correct_ranges=None,
-                 spline_ranges=None, smooth_win=0, mean_reference=True):
+                 spline_ranges=None, smooth_win=0, spline_base_win=9,
+                 mean_reference=True):
         if reference is None:
             raise MissingReferenceException()
         self.reference = reference
         self.correct_ranges = correct_ranges
         self.spline_ranges = spline_ranges
         self.smooth_win = smooth_win
+        self.spline_base_win = spline_base_win
         self.mean_reference = mean_reference
 
     def __call__(self, data):
@@ -135,6 +153,7 @@ class AtmCorr(Preprocess):
                           correct_ranges=self.correct_ranges,
                           spline_ranges=self.spline_ranges,
                           smooth_win=self.smooth_win,
+                          spline_base_win=self.spline_base_win,
                           mean_reference=self.mean_reference,
                           domain=data.domain)
         # takes care of domain column-wise, by above transformation function
