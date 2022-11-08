0a6323a16c61502ebf9f29d431df336525d2848e
Marko Toplak <marko.toplak@gmail.com>
2022-11-08 14:52:27 +0100

    Removed workaround for Orange<3.31

--- orangecontrib/spectroscopy/__init__.py
+++ orangecontrib/spectroscopy/__init__.py
@@ -2,15 +2,6 @@ import Orange.data
 import os.path
 
 
-# a no-op workaround so that table unlocking does not crash with older Orange
-# remove when the minimum supported version is 3.31
-from Orange.data import Table
-from contextlib import nullcontext
-if not hasattr(Table, "unlocked"):
-    Table.unlocked = nullcontext
-    Table.unlocked_reference = nullcontext
-
-
 from . import io  # register file formats
 
 
