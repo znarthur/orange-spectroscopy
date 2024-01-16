ea520440fda67f39ec5f481ed6c25c7bf7f6f3fa
Stuart Read <stuart.read@lightsource.ca>
2024-01-16 16:55:25 -0600

    wip

--- orangecontrib/spectroscopy/io/cls.py
+++ orangecontrib/spectroscopy/io/cls.py
@@ -12,3 +12,13 @@ class HDF5Reader_SGM(FileFormat, SpectralFileFormat):
 
     def read_spectra(self):
         with h5py.File(self.filename, 'r') as h5:
+            NXentries = [str(x) for x in h5['/'].keys() if 'NXentry' in str(h5[x].attrs.get('NX_class'))]
+            NXdata = [entry + "/" + str(x) for entry in NXentries for x in h5['/' + entry].keys()
+                      if 'NXdata' in str(h5[entry + "/" + x].attrs.get('NX_class'))]
+            axes = [[str(nm) for nm in h5[nxdata].keys() for s in h5[nxdata].attrs.get('axes') if str(s) in str(nm) or
+                     str(nm) in str(s)] for nxdata in NXdata]
+            indep_shape = [v.shape for i, d in enumerate(NXdata) for k, v in h5[d].items() if k in axes[i][0]]
+            data = [
+                {k: np.squeeze(v[()]) for k, v in h5[d].items() if v.shape[0] == indep_shape[i][0] and k not in axes[i]}
+                for i, d in
+                enumerate(NXdata)]
