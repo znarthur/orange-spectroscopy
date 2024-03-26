806bae3401060024e8b09c586e7cc77467c717e8
Marko Toplak <marko.toplak@gmail.com>
2024-03-26 16:01:55 +0100

    'doc' extras

--- setup.py
+++ setup.py
@@ -148,7 +148,8 @@ if __name__ == '__main__':
             'agilent-format>=0.4.5'
         ],
         extras_require={
-            'test': ['coverage']
+            'test': ['coverage'],
+            'doc': ['sphinx', 'recommonmark'],
         },
         entry_points=ENTRY_POINTS,
         keywords=KEYWORDS,
